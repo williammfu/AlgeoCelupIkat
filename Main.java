a8e2ffc58001e97f642a144f22bf6868ffa24cc3
rifaldyaristya <rifaldyaristya23@gmail.com>
2019-09-18 21:11:18 +0700

    editKurangiRow + bikin Gauss

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main{
     public static void main(String args[]){
-        MATRIKS M1 = new MATRIKS(3,3);
+        MATRIKS M1 = new MATRIKS(2,3);
         int i;
         i =  M1.GetFirstIdxBrs();
-        
         System.out.println(i);
+
+        M1.BacaMATRIKS();
+        //M1.TulisMATRIKS();
+        //M1.KurangiRow(2, 1,2);
+        //M1.KaliRow(2, 2);
+        //M1.TulisMATRIKS();
+        //M1.Swap(2, 3);
+        //M1.TulisMATRIKS(); 
+        M1.Gauss();
+        //M1.Pivotting(1);
+        M1.TulisMATRIKS();
     }
 }
