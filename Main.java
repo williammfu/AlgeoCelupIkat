17021c0cc112bcb7159907031cc7f8d38bcf6356
Anna Elvira H <naelvirahart@gmail.com>
2019-09-18 22:49:49 +0700

    main menu

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
+
+import java.util.Scanner; // untuk proses scan
+
+
 public class Main{
+    
     public static void main(String args[]){
+        public Scanner in = new Scanner(System.in); 
+
         MATRIKS M1 = new MATRIKS(2,3);
         int i;
         i =  M1.GetFirstIdxBrs();
+        
         System.out.println(i);
 
         M1.BacaMATRIKS();
@@ -15,5 +23,43 @@ public class Main{
         M1.Gauss();
         //M1.Pivotting(1);
         M1.TulisMATRIKS();
+
+        System.out.println("--------*** TUGAS BESAR 1 ALJABAR LINEAR & GEOMETRI ***--------");
+        System.out.println("----*** Sistem Persamaan Linier, Determinan, dan Aplikasinya ***----");
+
+        System.out.println("MENU");
+        System.out.println("1. Sistem Persamaan Linier");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks Balikan");
+        System.out.println("4. Matriks Kofaktor");
+        System.out.println("5. Adjoin");
+        System.out.println("6. Interpolasi Polinom");
+        System.out.println("7. Keluar");
+
+        System.out.println(" ");
+        System.out.print("Masukkan pilihan Anda: ");
+        
+    	
+    	int pilihan = in.nextInt();
+
+
+        System.out.println(" ");
+
+        while (pilihan != 7) {
+            switch (pilihan){
+                case 1 :
+                    System.out.println("1");
+                
+                break;
+            }
+        }
+
+        
+
+
+
+
+
+        
     }
 }
