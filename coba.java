958f15b434f30f2df23dfa5beb9960159eeea2a1
rifaldyaristya <rifaldyaristya23@gmail.com>
2019-09-21 09:38:30 +0700

    bikin read matriks dari file

--- coba.java
+++ coba.java
@@ -1,12 +1,23 @@
-import java.text.DecimalFormat; // untuk menampilkan 2(sbnrnya gk hrs 2) digit desimal
+
+import java.io.*;
+import java.util.*;
 
 public class coba{
-    private static DecimalFormat df2 = new DecimalFormat("#.##");
-    public static void main(String args[]){
-        double a = 0.00032;
-        double b = 83.1115434343;
-        a = Math.round(a*100.0)/100.0;
-        System.out.println(a);
-        System.out.println(a==0.0);
+    public void BacaFile() throws FileNotFoundException{
+    Scanner x = new Scanner (new File("chinese.txt"));
+
+                while(x.hasNext()){
+                    int a = x.nextInt();
+                    int b = x.nextInt();
+                    //int c = x.next();
+                    int sum = a+b;
+        
+                    System.out.printf("%d\n", sum);
+                }
+
+                x.close();
+            }
+    public void main(String args[]){
+        BacaFile();
     }
 }
